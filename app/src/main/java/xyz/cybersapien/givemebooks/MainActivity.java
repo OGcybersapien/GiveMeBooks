c9d762f79f99bef45759bfb6c0042ddb3d1e33c6
ogcybersapien <aditya@cybersapien.xyz>
2016-10-02 15:19:05 +0530

    fixed the bug where instance state wasn't saved on rotation

--- app/src/main/java/xyz/cybersapien/givemebooks/MainActivity.java
+++ app/src/main/java/xyz/cybersapien/givemebooks/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView hintingView;
     ListView resultsView;
+    Bundle onSaveInstanceState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
